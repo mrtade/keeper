e42cfde8ccf86c998450fc688ba3461f33942c1e
Leke Tade <leke.tade@gmail.com>
2020-07-25 10:31:35 +1000

    setup the addNote functionality

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function CreateArea() {
+  const [titleText, setTitleText] = useState('');
+  const [contentText, setContentText] = useState('');
+  const [notes, setNotes] = useState('');
+
+  function handleTitleChange(event) {
+    // const newTitleText = event.target.value;
+    setTitleText(event.target.value);
+  }
+
+  function handleContentChange(event) {
+    // const newContentText = event.target.value;
+    setContentText(event.target.value);
+  }
+
+  function addNote(event) {
+    event.preventDefault();
+    const id = Math.round(Math.random() * 999);
+    const entry = {
+      title: titleText,
+      content: contentText,
+      id: id,
+    };
+    setNotes(prevNotes => [...prevNotes, entry]);
+    // console.log(notes);
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          onChange={handleTitleChange}
+          name="title"
+          placeholder="Title"
+          value={titleText}
+        />
+        <textarea
+          onChange={handleContentChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={contentText}
+        />
+        <button onClick={addNote}>Add</button>
       </form>
     </div>
   );
